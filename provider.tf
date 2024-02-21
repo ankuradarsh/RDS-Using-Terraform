c6db34a42958f87be75a476646eacaf539def983
Ankur Adarsh <77184810+ankuradarsh@users.noreply.github.com>
2024-02-21 19:40:27 +0530

    Add files via upload

---
+++ provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+  access_key = "ASIAQGU7LMHUYJ24L72Z"
+  secret_key = "TSfGz7fpwa7u9yq4Zh4nf52dvJpWu+oQG+3FJM9j"
+  token = "FwoGZXIvYXdzEG0aDKd91/heEr/lVJlrDyKvAeF+k/OqbVpTnmv4JvlKT8S5RS48hOwgzsbNX4yh+zcV17TF3Io4tcJq6RiSZ4mSWuKDLM1e90f50phDZ9qYgmR6gWPK3qPpE8POO0WdlwKJ/Gbejn8LfxmGOBU0JcZtUvmyHlL+JD6jCpmpml/olUQkaPRl+hg7JakrTHCLbD35qYslR0SG/pnr/+85PI/usvWkkEzmpQDKxl7fI7AHAtigF1L6U0CEIjheJ5E3WjYooczXrgYyLUKglYYakXg24nNQWJzH3lYyWzYAYeE0mOCIuMK80dokxgCF9CioMlL5dTnyyw=="
+}
